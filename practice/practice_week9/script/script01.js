e4701a412e6f44d2cc7e436abc96cc356e4d0339
knahye <165612559+kimnahye0104@users.noreply.github.com>
2024-05-18 23:40:41 +0900

    개인연습 : <자바스크립트> : "연습 시작 (240518~)"

---
+++ practice/practice_week9/script/script01.js
@@ -0,0 +1,13 @@
+// 자바스크립트 복습
+var a = 1;
+console.log(a)
+var b = 2;
+console.log(a+b)
+b = 10;
+console.log(a+b)
+console.log(b-a)
+var coffee = 1500;
+var cookie = 500;
+console.log (`커피의 가격은 ${coffee}, 쿠기의 가격은 ${cookie}`)
+console.log(`총 가격은 ${coffee+cookie}`)
+console.log(`커피:${coffee} 1개와 쿠키:${cookie} 2개의 값은 ${coffee+(cookie*2)}`)
